19d287e9d2ddeb5ecad45d15d1ee2b6e94caf67d
王爽 <swang52133@gmail.com>
2017-10-26 19:03:03 +0800

    添加servlet

--- myservlert/src/main/java/com/ws/MyServlet.java
+++ myservlert/src/main/java/com/ws/MyServlet.java
@@ -1,5 +1,7 @@
 package com.ws;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,6 +14,24 @@ import java.io.IOException;
 public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletConfig config = getServletConfig();
         resp.getWriter().write("this is first servlet");
+        System.out.println("service");
+        System.out.println(config.getServletName());
+        System.out.println(config.getInitParameterNames());
+        System.out.println(config.getInitParameter("ws"));
+        System.out.println(config.getServletContext().toString());
+
+        ServletContext context = getServletContext();
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy");
+    }
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init");
     }
 }
---
+++ myservlert/src/main/java/com/ws/MyServlet_1.java
@@ -0,0 +1,19 @@
+package com.ws;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by ShuangWang on 2017/10/25.
+ */
+@WebServlet(name = "my", urlPatterns = "/me")
+public class MyServlet_1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("this is me");
+    }
+}
