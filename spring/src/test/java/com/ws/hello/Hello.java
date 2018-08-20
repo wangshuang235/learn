c650ef66446281e4298ca54a73c1f1b6516ad1f3
Miss.Lu <982838315@qq.com>
2018-08-21 00:26:22 +0800

    添加模块

---
+++ spring/src/test/java/com/ws/hello/Hello.java
@@ -0,0 +1,10 @@
+package com.ws.hello;
+
+public class Hello {
+    public String sayHello() {
+        return "Hello Maven";
+    }
+    public static void main(String[] args) {
+        System.out.println(new Hello().sayHello());
+    }
+}
